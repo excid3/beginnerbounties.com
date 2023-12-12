97bd29b23383d3c6d3e12908b985560be7e1eade
Chris Oliver <excid3@gmail.com>
2023-12-12 09:15:37 -0600

    Add mailpace delivery method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :mailpace
+  config.action_mailer.mailpace_settings = { api_token: Rails.application.credentials.mailpace_api_token }
+  config.action_mailer.default_url_options = { host: "beginnerbounties.com" }
 end
