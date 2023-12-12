80b3d5ad03c1df3f1ddb5ce5910b5cf85ca5a132
Morgan VanDerLeest <morganv@hey.com>
2023-12-12 16:50:06 -0600

    standardrb

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,23 +4,20 @@
 # Customize only what you really need and notice that the core Pagy works also without any of the following lines.
 # Should you just cherry pick part of this file, please maintain the require-order of the extras
 
-
 # Pagy DEFAULT Variables
 # See https://ddnexus.github.io/pagy/docs/api/pagy#variables
 # All the Pagy::DEFAULT are set for all the Pagy instances but can be overridden per instance by just passing them to
 # Pagy.new|Pagy::Countless.new|Pagy::Calendar::*.new or any of the #pagy* controller methods
 
-
 # Instance variables
 # See https://ddnexus.github.io/pagy/docs/api/pagy#instance-variables
 # Pagy::DEFAULT[:page]   = 1                                  # default
-Pagy::DEFAULT[:items]  = 10                                 # default
+Pagy::DEFAULT[:items] = 10                                 # default
 # Pagy::DEFAULT[:outset] = 0                                  # default
-
 
 # Other Variables
 # See https://ddnexus.github.io/pagy/docs/api/pagy#other-variables
-Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
+Pagy::DEFAULT[:size] = [1, 1, 1, 1]                       # default
 # Pagy::DEFAULT[:page_param] = :page                           # default
 # The :params can be also set as a lambda e.g ->(params){ params.exclude('useless').merge!('custom' => 'useful') }
 # Pagy::DEFAULT[:params]     = {}                              # default
@@ -30,10 +27,8 @@ Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
 # Pagy::DEFAULT[:cycle]      = true                            # example
 # Pagy::DEFAULT[:request_path] = "/foo"                        # example
 
-
 # Extras
 # See https://ddnexus.github.io/pagy/categories/extra
-
 
 # Backend Extras
 
@@ -122,7 +117,6 @@ Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
 # uncomment if you are going to use Searchkick.pagy_search
 # Searchkick.extend Pagy::Searchkick
 
-
 # Frontend Extras
 
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
@@ -158,7 +152,6 @@ Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
 # Multi size var used by the *_nav_js helpers
 # See https://ddnexus.github.io/pagy/docs/extras/navs#steps
 # Pagy::DEFAULT[:steps] = { 0 => [2,3,3,2], 540 => [3,5,5,3], 720 => [5,7,7,5] }   # example
-
 
 # Feature Extras
 
@@ -196,7 +189,6 @@ Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
 # See https://ddnexus.github.io/pagy/docs/extras/standalone
 # require 'pagy/extras/standalone'
 # Pagy::DEFAULT[:url] = 'http://www.example.com/subdir'  # optional default
-
 
 # Rails
 # Enable the .js file required by the helpers that use javascript
@@ -236,7 +228,6 @@ Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
 #                   filepath: 'path/to/pagy-xyz.yml',
 #                   pluralize: lambda{ |count| ... } )
 
-
 # I18n extra: uses the standard i18n gem which is ~18x slower using ~10x more memory
 # than the default pagy internal i18n (see above)
 # See https://ddnexus.github.io/pagy/docs/extras/i18n
@@ -244,7 +235,6 @@ Pagy::DEFAULT[:size]       = [1,1,1,1]                       # default
 
 # Default i18n key
 # Pagy::DEFAULT[:i18n_key] = 'pagy.item_name'   # default
-
 
 # When you are done setting your own default freeze it, so it will not get changed accidentally
 Pagy::DEFAULT.freeze
