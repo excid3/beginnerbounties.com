181b2d716bd8ef36d5b8869e38ed69abcb380734
Evan Sparkman <sparkman.evan@gmail.com>
2024-08-12 09:01:24 -0400

    Restore Prod mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :mailpace
+  config.action_mailer.mailpace_settings = {api_token: Rails.application.credentials.mailpace_api_token}
+  config.action_mailer.default_url_options = {host: "beginnerbounties.com"}
 end
